d0c62495d3ac1fbceefc33bd7cd5c190063e6a41
Kang Junhyeok <139138763+00KANGJK@users.noreply.github.com>
2024-09-05 21:21:22 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
-
+//두 숫자 입력 받는다.
+//charAt() 이용해서 하나씩 곱하고 출력해줌.
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        System.out.println(num1 *(num2%10));
-        System.out.println(num1 *(num2%100/10));
-        System.out.println(num1 *(num2/100));
-        System.out.println(num1 * num2);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String b = sc.next();
         
-    }
+        for(int i=2; i>=0; i--){
+            int c = (int)(b.charAt(i)-'0');
+            System.out.println(a*c);
+        }
+        
+        System.out.println(a * Integer.parseInt(b));
+	}
 }
