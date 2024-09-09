c143576d1d13f8f91a8d7e72d83ea1d4aaacb77e
Kang Junhyeok <139138763+00KANGJK@users.noreply.github.com>
2024-09-10 01:36:40 +0900

    [Bronze II] Title: 공 바꾸기, Time: 192 ms, Memory: 18484 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
+//처음 입력받는 두 수, 총 바구니 수 / 총 바꿀 횟수
+//순서대로 ball의 번호를 넣어 배열을 만든다.
+//바꿀 두 번호를 입력받아. 서로 교환해준다.
 
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int basket = sc.nextInt();
-        int chance = sc.nextInt();
-        int[] arr = new int[basket];
-        for(int i =0; i < basket; i++) {
-            arr[i] = i+1;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int[] ball = new int[a];
+        for(int i =0; i< a; i++){
+            ball[i] = i+1;
         }
-
-        for(int i =0; i<chance; i++){
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
-            int exchange = arr[num1-1];
-            arr[num1-1] = arr[num2-1];
-            arr[num2-1] = exchange;
+        
+        for(int i =0; i< b; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int change = ball[x-1];
+            ball[x-1] = ball[y-1];
+            ball[y-1] = change;
         }
-        for(int i =0; i < basket; i++) {
-            System.out.print(arr[i] + " ");
+        for(int i=0; i< ball.length; i++){
+            System.out.print(ball[i]+" ");
         }
-    }
+	}
 }
