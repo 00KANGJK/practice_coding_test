98095e60028588ba4526f654da4f79317c6f0223
Kang Junhyeok <139138763+00KANGJK@users.noreply.github.com>
2024-09-10 13:13:26 +0900

    [Bronze V] Title: 합, Time: 164 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,14 +1,12 @@
-import java.util.Scanner;
+//합을 구하는 것은 그냥 등차가 1인 수열의 합 구하는 방법
 
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        // Your code here
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sum = 0;
-        for(int i=1; i<=n; i++){
-            sum += i;
-        }
-        System.out.print(sum);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int total = a*(1+a)/2;
+        
+        System.out.println(total);
+	}
 }
