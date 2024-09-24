7e1032b6d1c1b9d5869640743948053a473c41db
Kang Junhyeok <139138763+00KANGJK@users.noreply.github.com>
2024-09-24 15:17:55 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
-        while(true){
-            if(n<a) {
-                break;
-            }
+        while(n>=a){
             answer += (n/a)*b;
-            int remain = n%a;
-            n = (n/a)*b + remain;
+            n = (n/a)*b + n%a;
         }
         return answer;
     }
 }
+//answer에 돌려받은 콜라병의 갯수(n)를 받는다.
+//총 콜라병 갯수(n)를 최신화 해줌.
