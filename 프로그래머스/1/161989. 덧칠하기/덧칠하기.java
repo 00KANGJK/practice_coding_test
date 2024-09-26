60138b7b0fd11aa95916519051adc627509ae0f2
Kang Junhyeok <139138763+00KANGJK@users.noreply.github.com>
2024-09-26 10:40:08 +0900

    [level 1] Title: 덧칠하기, Time: 2.01 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,19 +1,22 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int start = section[0];
-        int end = section[0] + (m-1);
         int ans = 1;
-            
-        for(int i : section){
-            if(i>=start && i<=end)
-                continue;
-            else{
-                start = i;
-                end = i + (m-1);
+        int start = section[0];
+        // if(start+m-1 >= section[section.length-1]) return 1;
+        
+        for(int i = 1; i<section.length; i++){
+            if(start + m-1<section[i]){
                 ans++;
+                start=section[i];
             }
-            
         }
+
         return ans;
     }
 }
+// 벽을 n개로 나눔(1m), 롤러의 길이는 m, 다시 칠하기 위한 구역 번호 section
+// 벽을 칠하는 횟수를 최소화하려고 함.
+// section이 정렬되어 있기 때문에 section의 처음과 끝을 저장해줌.
+// -> 이유는 굳이 1번 구역부터 할 필요가 없음. 
+// 그리고 처음에 m을 더했을때 마지막 벽보다 크거나 같으면 과정을 더 할 필요없다.
+// start가 section보다 작거나 같을 때 카운트 하고 m을 더해준다.
